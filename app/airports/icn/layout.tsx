1071e043733e98d4c701e0b84fdaa2277861ca93
christmas725 <88099315+christmas725@users.noreply.github.com>
2026-09-03 08:36:00 +0900

    feat: automate ICN and TAE source synchronization

--- app/airports/icn/layout.tsx
+++ app/airports/icn/layout.tsx
@@ -3,6 +3,8 @@ import "./mobile.css";
 import "./foldable.css";
 import "./desktop-height.css";
 import "./destination-scroll.css";
+import "./tablet.css";
+import "./adaptive-row.css";
 
 export default function IcnLayout({ children }: { children: React.ReactNode }) {
   return children;
