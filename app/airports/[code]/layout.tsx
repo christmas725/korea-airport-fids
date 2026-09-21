b92bb23b9b63f5726e31663a22cf1cabbc51b270
christmas725 <88099315+christmas725@users.noreply.github.com>
2026-09-21 09:00:24 -0400

    refactor: use shared airline logo handling across KAC FIDS

--- app/airports/[code]/layout.tsx
+++ app/airports/[code]/layout.tsx
@@ -1,15 +1,16 @@
-import KacAirlineLogoSync from "@/components/fids/KacAirlineLogoSync";
+import AirlineLogoSync from "@/components/fids/AirlineLogoSync";
 import "../tae/tae.css";
 import "../tae/mobile.css";
 import "../tae/foldable.css";
 import "../fids-common.css";
 import "../tae/clock.css";
 import "../tae/airline-copy.css";
+import "../airline-logo.css";
 
 export default function KacAirportLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <KacAirlineLogoSync />
+      <AirlineLogoSync />
       {children}
     </>
   );
