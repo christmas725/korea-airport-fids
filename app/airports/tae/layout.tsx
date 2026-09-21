fbaa57663d40d6dc1b226c4d7775ac4f7337f464
christmas725 <88099315+christmas725@users.noreply.github.com>
2026-09-21 08:19:53 -0400

    style: apply Incheon-sized clock to Daegu FIDS

--- app/airports/tae/layout.tsx
+++ app/airports/tae/layout.tsx
@@ -2,6 +2,7 @@ import "./tae.css";
 import "./mobile.css";
 import "./foldable.css";
 import "../fids-common.css";
+import "./clock.css";
 
 export default function TaeLayout({ children }: { children: React.ReactNode }) {
   return children;
