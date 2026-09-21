42d9c7e546b8992a72ed5bcd954e23da4deb145b
christmas725 <88099315+christmas725@users.noreply.github.com>
2026-09-21 08:44:03 -0400

    style: load shared KAC airline copy styles

--- app/airports/[code]/layout.tsx
+++ app/airports/[code]/layout.tsx
@@ -3,6 +3,7 @@ import "../tae/mobile.css";
 import "../tae/foldable.css";
 import "../fids-common.css";
 import "../tae/clock.css";
+import "../tae/airline-copy.css";
 
 export default function KacAirportLayout({ children }: { children: React.ReactNode }) {
   return children;
